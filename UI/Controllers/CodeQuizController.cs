175a19331d75e938f076bb874f8928278ab68502
lakshmibhargavab <lakshmibhargavab@proarch.com>
2017-06-19 20:07:22 +0530

    Intial Commit

---
+++ UI/Controllers/CodeQuizController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UI.Models;
+
+namespace UI.Controllers
+{
+    public class CodeQuizController : Controller
+    {
+        // GET: CodeQuiz
+        public ActionResult Index()
+        {
+
+            return View(new DataMethods().GetCodeQuizzes());
+        }
+    }
+}
---
+++ UI/Models/DataMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models
+{
+    public class DataMethods
+    {
+        public static string connectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ToString();
+        public List<CodeQuiz> GetCodeQuizzes()
+        {
+            var codeQuizzes = new List<CodeQuiz>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand("CQ_GetAllCodeQuizzes", connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    {
+                        codeQuizzes.Add(new CodeQuiz() {
+                            Title = reader["title"].ToString(),
+                            Description = reader["description"].ToString(),
+                            StartTime = DateTime.Parse( reader["starttime"].ToString()),
+                            EndTime = DateTime.Parse(reader["endtime"].ToString())
+                        });
+                    }
+                }
+            }
+
+            return codeQuizzes;
+        }
+
+    }
+}
